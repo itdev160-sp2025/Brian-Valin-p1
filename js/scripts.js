2ea1ce10285c94ceadda10a84a82bcd6430f9bda
Valin <212599415@gehealthcare.com>
2025-03-04 10:31:26 -0600

    Complete Project 1

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var user = 'Brian';
+var salutation = 'Welcome Back, ';
+var pageViews = "2,369"
+var greeting = salutation + user + "! You have viewed this page " + pageViews + " times!";
+var greetingEl = document.getElementById('greeting');
+
+greetingEl.textContent = greeting;
+
+var Rank1 = "Mazda MX-5 Miata"
+    Rank1El = document.getElementById('Rank1');
+    Rank1El.textContent = Rank1
+
+var Rank2 = "Toyota GR86"
+    Rank2El = document.getElementById('Rank2');
+    Rank2El.textContent = Rank2
+
+var Rank3 = "Suburu BRZ"
+    Rank3El = document.getElementById('Rank3');
+    Rank3El.textContent = Rank3
